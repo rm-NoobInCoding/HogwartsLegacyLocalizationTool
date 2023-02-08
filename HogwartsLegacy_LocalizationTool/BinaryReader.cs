7f36944ef0aa59d05f9d9ba2fca033429d99742f
win10 <win10@DESKTOP-BO7QSDR>
2023-02-08 16:14:27 +0330

    Add project files.

---
+++ HogwartsLegacy_LocalizationTool/BinaryReader.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HogwartsLegacy_LocalizationTool
+{
+    internal class BinaryReader : System.IO.BinaryReader
+    {
+        public BinaryReader(System.IO.Stream stream) : base(stream) { }
+
+        public string Margic(int size)
+        {
+            return Encoding.Default.GetString(ReadBytes(size));
+        }
+        public string ReadStringAtOffset(long offset, int length)
+        {
+            long pos = base.BaseStream.Position;
+            base.BaseStream.Seek(offset,SeekOrigin.Begin);
+            string ret = String.Concat(ReadChars(length));
+            base.BaseStream.Seek(pos, SeekOrigin.Begin);
+            return ret;
+        }
+
+        public void Skip(int To)
+        {
+            base.BaseStream.Seek(To, System.IO.SeekOrigin.Current);
+        }
+
+        public void Seek(int To)
+        {
+            base.BaseStream.Seek(To, System.IO.SeekOrigin.Begin);
+        }
+
+        public void Seek(uint To)
+        {
+            base.BaseStream.Seek(To, System.IO.SeekOrigin.Begin);
+        }
+
+        public byte[] ReadToEnd()
+        {
+            return base.ReadBytes((int)base.BaseStream.Length - (int)base.BaseStream.Position);
+        }
+
+        public void Pos(int Base)
+        {
+            base.BaseStream.Position = Base;
+        }
+
+        public UInt64[] ReadULonges(int Size)
+        {
+            List<UInt64> ulongs = new List<UInt64>();
+            for(int i = 0; i < Size; i++)
+            {
+                ulongs.Add(ReadUInt64());
+            }
+            return ulongs.ToArray();
+        }
+
+        public UInt32[] ReadUInts(int Size)
+        {
+            List<UInt32> uints = new List<UInt32>();
+            for (int i = 0; i < Size; i++)
+            {
+                uints.Add(ReadUInt32());
+            }
+            return uints.ToArray();
+        }
+
+        public long Tell()
+        {
+            return base.BaseStream.Position;
+        }
+    }
+}
---
+++ HogwartsLegacy_LocalizationTool/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HogwartsLegacy_LocalizationTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Bin Files|*.bin"
+            };
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                BinaryReader rd = new BinaryReader(new MemoryStream(File.ReadAllBytes(ofd.FileName)));
+                long CountOfStrings, HeaderSize, StartOfStringsWithoutHeader, StartOfStrings, SizeOfStringsBlock;
+                string Magic, ID, Str;
+                Magic = Encoding.Unicode.GetString(rd.ReadBytes(32));
+                CountOfStrings = rd.ReadInt64();
+                HeaderSize = rd.ReadInt64();
+                StartOfStringsWithoutHeader = rd.ReadInt64();
+                StartOfStrings = rd.ReadInt64();
+                SizeOfStringsBlock = rd.ReadInt64();
+                List<string> Strings = new List<string>();
+                List<string> IDs= new List<string>();
+                for(int i = 0;i< CountOfStrings; i++)
+                {
+                    long StringOffset = rd.ReadInt64();
+                    int StringLen = rd.ReadInt32();
+                    ID = rd.ReadStringAtOffset(StringOffset + StartOfStrings, StringLen);
+                    IDs.Add(RemoveNewLine(ID));
+
+                    StringOffset = rd.ReadInt64();
+                    StringLen = rd.ReadInt32();
+                    Str = rd.ReadStringAtOffset(StringOffset + StartOfStrings, StringLen);
+                    Strings.Add(RemoveNewLine(Str));
+                }
+                rd.Close();
+                File.WriteAllLines(ofd.FileName+".txt", Strings.ToArray());
+                File.WriteAllLines(ofd.FileName + ".ids", IDs.ToArray());
+                MessageBox.Show("Done!");
+
+            }
+
+        }
+        private string RemoveNewLine(string str)
+        {
+            string ret = str;
+            ret = ret.Replace("\r\n", "<cf>");
+            ret = ret.Replace("\n", "<lf>");
+            ret = ret.Replace("\r", "<cr>");
+            return ret;
+        }
+        private string AddNewLine(string str)
+        {
+            string Text = str;
+            Text = Text.Replace("<cf>", "\r\n");
+            Text = Text.Replace("<lf>", "\n");
+            Text = Text.Replace("<cr>", "\r");
+            return Text;
+        }
+
+    }
+}
